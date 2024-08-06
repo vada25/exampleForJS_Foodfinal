dda7f11d7d129a0e1856220f28046ed8ec4ea9a3
vada25 <vada1981@gmail.com>
2024-08-06 17:01:36 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -182,12 +182,12 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    getResource('http://localhost:3000/menu')
+   /*  getResource('http://localhost:3000/menu')
         .then(data => {
             data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
             });
-        });
+        }); */
 
     // getResource('http://localhost:3000/menu')
     //     .then(data => createCard(data));
@@ -211,6 +211,12 @@ window.addEventListener('DOMContentLoaded', function() {
     //         document.querySelector(".menu .container").append(element);
     //     });
     // }
+    axios.get('http://localhost:3000/menu')
+        .then(data =>  {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+        });
+    });
 
     // Forms
 
@@ -298,4 +304,52 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }, 4000);
     }
+
+    // Slider
+
+    let slideIndex = 1;
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+        
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    }
+
+    function plusSlides (n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
 });
