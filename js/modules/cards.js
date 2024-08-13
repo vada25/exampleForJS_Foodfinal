69d4911ceaff12f14e636d7689b1402d6b19a405
vada25 <vada1981@gmail.com>
2024-08-13 11:27:16 +0300

    upd

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -1,3 +1,4 @@
+import { getResource } from "../services/services";
 function cards() {
     // Используем классы для создание карточек меню
 
@@ -49,15 +50,7 @@ function cards() {
             });
         });
 
-    async function getResource(url) {
-        let res = await fetch(url);
-    
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-    
-        return await res.json();
-    }
+
 }
 
-module.exports = cards;
+export default cards;
--- script.js
+++ script.js
@@ -1,19 +1,29 @@
+import tabs from './js/modules/tabs';
+import calc from './js/modules/calc';
+import cards from './js/modules/cards';
+import forms from './js/modules/forms';
+import modal from './js/modules/modal';
+import slider from './js/modules/slider';
+import timer from './js/modules/timer';
+import {openModal} from './js/modules/modal';
+
 window.addEventListener('DOMContentLoaded', function() {
-    const tabs = require('./js/modules/tabs'),
-        calc = require('./js/modules/calc'),
-        cards = require('./js/modules/cards'),
-        forms = require('./js/modules/forms'),
-        modal = require('./js/modules/modal'),
-        slider = require('./js/modules/slider'),
-        timer = require('./js/modules/timer');
-    
-    tabs();
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     calc();
     cards();
-    forms();
-    modal();
-    slider();
-    timer();
-
+    forms('form', modalTimerId);
+    modal('[data-modal]', '.modal');
+    slider({
+        container: '.offer__slider', 
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        wrapper: '.offer__slider-wrapper',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        field: '.offer__slider-inner',
+        slide: '.offer__slide'
+    });
+    timer('.timer', '2024-09-15');
 });
 
